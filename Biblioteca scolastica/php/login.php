e0ddb732769d85acfcbd12c84ad4644cb3de7018
sezz00 <gabriele.severi@studenti.itisarezzo.it>
2026-05-21 16:03:00 +0200

    Login
    Pagina login da associare alla registrazione e foglio .txt

---
+++ Biblioteca scolastica/php/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+   
+    <form method="POST" action="">
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+        <br>
+
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        <br>
+
+        <input type="submit" value="Accedi">
+        
+    </form>
+    
+</body>
+</html>
